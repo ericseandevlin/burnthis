8a7262c3ff99535506a005edcfd812df0b5b37f0
Eric <ericd@hellorpm.com>
2018-09-28 11:09:47 -0400

    Add favicons

--- wp-content/themes/burnthis/header.php
+++ wp-content/themes/burnthis/header.php
@@ -2,7 +2,7 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Burn This</title>
 
 
 <!-- Google Tag Manager -->
@@ -10,8 +10,17 @@
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','<TAG-MANAGER-ID>');</script>
+})(window,document,'script','dataLayer','GTM-PJ4V79S');</script>
 <!-- End Google Tag Manager -->
+
+
+<link rel="apple-touch-icon" sizes="180x180" href="<?= get_template_directory_uri(); ?>/favicons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?= get_template_directory_uri(); ?>/favicons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?= get_template_directory_uri(); ?>/favicons/favicon-16x16.png">
+<link rel="manifest" href="<?= get_template_directory_uri(); ?>/favicons/site.webmanifest">
+<link rel="mask-icon" href="<?= get_template_directory_uri(); ?>/favicons/safari-pinned-tab.svg" color="#2e333b">
+<meta name="msapplication-TileColor" content="#2b5797">
+<meta name="theme-color" content="#ffffff">
 
 
 <?php wp_head(); ?>
@@ -28,7 +37,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 <body <?php body_class("body"); ?>>
 
   <!-- Google Tag Manager (noscript) -->
-  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<TAG-MANAGER-ID>"
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PJ4V79S"
   height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
   <!-- End Google Tag Manager (noscript) -->
 
