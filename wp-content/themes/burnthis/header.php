bb403df4ce40f89c6fb317555168c0383e4da267
rachelle <leahrachelle.w@gmail.com>
2018-09-28 13:13:37 -0400

    initial

--- wp-content/themes/burnthis/header.php
+++ wp-content/themes/burnthis/header.php
@@ -2,7 +2,7 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Burn This Broadway</title>
 
 
 <!-- Google Tag Manager -->
--- wp-content/themes/burnthis/templates/front-page.php
+++ wp-content/themes/burnthis/templates/front-page.php
@@ -7,9 +7,36 @@
 
 <?php get_header(); ?>
 
-<h1>Front Page</h1>
+  <div class="main-container">
+    <h1 class="name">ADAM <br>DRIVER</h1>
+    <h1 class="keri">KERI <br>RUSSELL</h1>
+    <h2 class="byline">LANFORD <br>WILSON’S</h2>
+    <h1 class="title">BURN<br>THIS</h1>
+    <h2 class="role">DIRECTED BY</h2>
+    <h2 class="byline">MICHAEL<br>MAYER</h2>
+    <h2 class="date">ON BROADWAY <br>MARCH 2019</h2>
 
- <?php //get_template_part(''); ?>
+
+    <div class="form-block w-form">
+
+      <!-- Begin Mailchimp Signup Form -->
+
+      <form action="https://HelloRPM.us15.list-manage.com/subscribe/post?u=3e0c0ff24e6c34445bab47703&amp;id=41897d17f7" method="post" id="mc-embedded-subscribe-form email-form" name="mc-embedded-subscribe-form" class="validate form" target="_blank" novalidate>
+          <div id="mc_embed_signup_scroll">
+        <h2 class="email-label">SUBSCRIBE</h2>
+
+      	<div class="email-input"><input type="email" value="" name="EMAIL" class=" text-field w-input mail" id="mce-EMAIL" placeholder="email address" required>
+          <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+          <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_3e0c0ff24e6c34445bab47703_41897d17f7" tabindex="-1" value=""></div>
+          <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button send-button w-button"></div>
+          </div></div>
+
+          </form>
+
+      <!--End mc_embed_signup-->
+
+    </div>
+    <div class="social-holder"><a href="#" class="social-link w-inline-block"><img src="images/icon_facebook_white.svg" width="35"></a><a href="#" class="social-link w-inline-block"><img src="images/icon_twitter_white.svg" width="35"></a><a href="#" class="social-link w-inline-block"><img src="images/icon_instagram.svg" width="35"></a></div><a href="#" class="footer-link">Privacy Policy</a></div>
 
 
 <?php get_footer(); ?>
