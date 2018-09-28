a088a092043cbdb03a8d8ee6c038e3f2edbc6fda
rachelle <leahrachelle.w@gmail.com>
2018-09-28 13:40:30 -0400

    hookup mailchimp

---
+++ wp-content/themes/burnthis/partials/signup.php
@@ -0,0 +1,63 @@
+
+
+<?php $signup = 'modal'; ?>
+
+
+
+<!-- Begin MailChimp Signup Form -->
+<div id="mc_embed_signup" class="email-form <?= $signup ?> form-2">
+  <!-- <div class="form-block w-form"> -->
+
+    <form action="https://HelloRPM.us15.list-manage.com/subscribe/post?u=3e0c0ff24e6c34445bab47703&amp;id=41897d17f7" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+
+      <div id="mc_embed_signup_scroll" class="form form-2 <?= $signup ?>">
+
+        <div class="mc-field-group">
+          <!-- email field -->
+          <input type="email" value="" name="EMAIL" class="text-field-2 w-input" id="mce-EMAIL" placeholder="yourname@email.com">
+        </div>
+
+        <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+        <div style="position: absolute; left: -5000px;" aria-hidden="true">
+          <input type="text" name="b_3e0c0ff24e6c34445bab47703_1d17011b5c" tabindex="-1" value="">
+        </div>
+
+        <!-- <label> -->
+        <div class="submit-holder">
+          <input type="submit" value="SIGN UP" name="subscribe" id="mc-embedded-subscribe" class="button w-button submit-button <?= $signup ?>">
+        </div>
+        <!-- </label> -->
+
+      </div>
+
+      <div id="mce-responses" class="clear">
+        <div class="response" id="mce-error-response" style="display:none"></div>
+        <div class="response" id="mce-success-response" style="display:none"></div>
+      </div>
+
+    </form>
+
+  <!-- </div> -->
+</div>
+<!--End mc_embed_signup-->
+
+
+<!-- <div class="email-form w-form">
+
+  <form id="wf-form-Email-Form" name="wf-form-Email-Form" data-name="Email Form" class="form-2">
+
+    <input type="email" class="text-field-2 w-input email-signup" maxlength="256" name="email-field-2" data-name="Email Field 2" placeholder="yourname@email.com" id="email-field-2" required="">
+
+    <input type="submit" value="SIGN UP" data-wait="Please wait..." class="submit-button w-button">
+
+  </form>
+
+  <div class="w-form-done">
+    <div>Thank you! Your submission has been received!</div>
+  </div>
+
+  <div class="w-form-fail">
+    <div>Oops! Something went wrong while submitting the form.</div>
+  </div>
+
+</div> -->
--- wp-content/themes/burnthis/templates/front-page.php
+++ wp-content/themes/burnthis/templates/front-page.php
@@ -18,25 +18,11 @@
 
 
     <div class="form-block w-form">
-
-      <!-- Begin Mailchimp Signup Form -->
-
-      <form action="https://HelloRPM.us15.list-manage.com/subscribe/post?u=3e0c0ff24e6c34445bab47703&amp;id=41897d17f7" method="post" id="mc-embedded-subscribe-form email-form" name="mc-embedded-subscribe-form" class="validate form" target="_blank" novalidate>
-          <div id="mc_embed_signup_scroll">
-        <h2 class="email-label">SUBSCRIBE</h2>
-
-      	<div class="email-input"><input type="email" value="" name="EMAIL" class=" text-field w-input mail" id="mce-EMAIL" placeholder="email address" required>
-          <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-          <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_3e0c0ff24e6c34445bab47703_41897d17f7" tabindex="-1" value=""></div>
-          <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button send-button w-button"></div>
-          </div></div>
-
-          </form>
-
-      <!--End mc_embed_signup-->
+          <?php get_template_part('partials/signup') ?>
 
     </div>
-    <div class="social-holder"><a href="#" class="social-link w-inline-block"><img src="images/icon_facebook_white.svg" width="35"></a><a href="#" class="social-link w-inline-block"><img src="images/icon_twitter_white.svg" width="35"></a><a href="#" class="social-link w-inline-block"><img src="images/icon_instagram.svg" width="35"></a></div><a href="#" class="footer-link">Privacy Policy</a></div>
+
+    <a href="#" class="footer-link">Privacy Policy</a></div>
 
 
 <?php get_footer(); ?>
