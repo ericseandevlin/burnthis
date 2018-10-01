ebbd7017bd4dc88ee7534f6bcfa80f32f018b79e
rachelle <leahrachelle.w@gmail.com>
2018-10-01 14:41:57 -0400

    404

--- wp-content/themes/burnthis/404.php
+++ wp-content/themes/burnthis/404.php
@@ -8,18 +8,11 @@
 
  <div class="main-content">
    <div class="container w-container">
-
-       <div class="container-2 _404 w-container">
-
-         <img src="https://d3e54v103j8qbb.cloudfront.net/static/page-not-found.211a85e40c.svg" class="image">
-
-         <h1 class="heading-2">404</h1>
+         <h1 class="_404-head">404</h1>
 
          <div class="_404-text">THE PAGE YOU ARE LOOKING FOR DOESN&#x27;T EXIST OR HAS BEEN MOVED.</div>
-         <a href="<?= home_url(); ?>" class="home-link transition">RETURN HOME</a>
 
-       </div>
-
+         <a href="<?= home_url(); ?>" class="footer-link">RETURN HOME</a>
 
    </div>
  </div>
