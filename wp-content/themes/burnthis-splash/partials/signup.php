3ef4a398941f50ad03c9f8b2b760abc53ec100cc
rachelle <leahrachelle.w@gmail.com>
2018-11-20 14:22:26 -0500

    Burn This Announcement Splash

--- wp-content/themes/burnthis-splash/partials/signup.php
+++ wp-content/themes/burnthis-splash/partials/signup.php
@@ -8,9 +8,8 @@
     <form action="https://HelloRPM.us15.list-manage.com/subscribe/post?u=3e0c0ff24e6c34445bab47703&amp;id=41897d17f7" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate form" target="_blank" novalidate>
 
       <div id="mc_embed_signup_scroll" class="form form-2 <?= $signup ?>">
-        <div class="mc-field-group email-input">
           <!-- email field -->
-          <input type="email" value="" name="EMAIL" class="text-field w-input" id="mce-EMAIL" placeholder="EMAIL ADDRESS">
+          <input type="email" value="" name="EMAIL" class="signup-input" id="mce-EMAIL" placeholder="EMAIL ADDRESS">
 
         <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
         <div style="position: absolute; left: -5000px;" aria-hidden="true">
@@ -18,10 +17,8 @@
         </div>
 
         <!-- <label> -->
-          <input type="submit" value="SIGN UP" name="subscribe" id="mc-embedded-subscribe" class="button w-button submit-button send-button">
+          <input type="submit" value="SIGN UP" name="subscribe" id="mc-embedded-subscribe" class="submit-button send-button">
         <!-- </label> -->
-        </div>
-
       </div>
 
       <div id="mce-responses" class="clear">
--- wp-content/themes/burnthis-splash/templates/front-page.php
+++ wp-content/themes/burnthis-splash/templates/front-page.php
@@ -7,21 +7,37 @@
 
 <?php get_header(); ?>
 
-  <div class="main-container">
-    <h1 class="name">ADAM <br>DRIVER</h1>
-    <h1 class="keri">KERI <br>RUSSELL</h1>
-    <h2 class="byline">LANFORD <br>WILSON’S</h2>
-    <h1 class="title">BURN<br>THIS</h1>
-    <h2 class="role">DIRECTED BY</h2>
-    <h2 class="byline">MICHAEL<br>MAYER</h2>
-    <h2 class="date">ON BROADWAY MARCH 2019</h2>
-
-
-          <?php get_template_part('partials/signup') ?>
-
-
-    <a href="/privacy" class="footer-link">Privacy Policy</a></div>
-
+  <div class="push-footer">
+    <div class="main-wrapper">
+      <div class="header-wrapper"><img src="<?= get_template_directory_uri(); ?>/_images/burnthis_hero_image_desktop.jpg" alt="" class="hero-desktop"><img src="<?= get_template_directory_uri(); ?>/_images/burnthis_hero_image_mobile.jpg" alt="" class="hero-mobile"></div>
+      <div class="content-wrapper">
+        <div class="main-content">
+          <div class="preview-wrapper">
+            <p class="preview-line"><span class="pre-bold">PREVIEWS BEGIN</span> MARCH 15, 2019</p>
+            <p class="preview-line"><span class="pre-bold">OPENS</span> APRIL 15, 2019</p>
+          </div>
+          <div class="presale-wrapper">
+            <p class="presale-line"><span class="bold text-span">american express presale<br></span>nov 28 10:00 am - nov 30 9:59 am</p>
+            <p class="presale-line"><span class="bold text-span">AUDIENCE REWARDS PRESALE<br>‍</span>nov 30 10:00 am - dec 2 9:59 am</p>
+            <p class="presale-line"><span class="bold text-span">General Onsale<br></span>dec 2 10:00 am</p>
+          </div>
+          <div class="signup-wrapper">
+            <div class="form-wrapper w-form">
+            	<?php
+        				include(locate_template('partials/signup.php'));
+        		 ?>
+            </div>
+          </div>
+          <div class="theater-wrapper">
+            <p class="theater-line"><a href="https://goo.gl/maps/fsF2jfZkZbk" target="_blank" class="link link-2"><span class="bold text-span-2">HUDSON THEATRE</span></a> 141 W 44TH ST, NEW YORK, NY 10036</p>
+          </div>
+        </div>
+      </div>
     </div>
+    <div class="footer-wrapper">
+      <div class="social-wrapper"><a href="#" class="social-link w-inline-block"><img src="<?= get_template_directory_uri(); ?>/_images/icon_fb.svg" height="35" alt="Burn This Broadway Facebook" width="34.5" class="fb-img"></a><a href="#" class="social-link w-inline-block"><img src="<?= get_template_directory_uri(); ?>/_images/icon_tw.svg" height="28" alt="Burn This Broadway Twitter" width="91" class="twitter-img"></a><a href="#" class="social-link w-inline-block"><img src="<?= get_template_directory_uri(); ?>/_images/icon_ig.svg" height="35" alt="Burn This Broadway Instagram" width="75" class="insta-img"></a></div>
+      <div class="amex-wrapper"><img src="<?= get_template_directory_uri(); ?>/_images/amex_lockup.png" width="175" alt="American Express Proud Partner of Burn This" class="amex-img"></div>
+    </div>
+  </div>
 
 <?php get_footer(); ?>
