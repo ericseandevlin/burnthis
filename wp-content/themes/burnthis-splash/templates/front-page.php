5645c2a852c5f99f112a1ded57f7bb636705f3af
rachelle <leahrachelle.w@gmail.com>
2018-11-26 14:24:15 -0500

    ACF fields social

--- wp-content/themes/burnthis-splash/templates/front-page.php
+++ wp-content/themes/burnthis-splash/templates/front-page.php
@@ -35,7 +35,15 @@
       </div>
     </div>
     <div class="footer-wrapper">
-      <div class="social-wrapper"><a href="#" class="social-link w-inline-block"><img src="<?= get_template_directory_uri(); ?>/_images/icon_fb.svg" height="35" alt="Burn This Broadway Facebook" width="34.5" class="fb-img"></a><a href="#" class="social-link w-inline-block"><img src="<?= get_template_directory_uri(); ?>/_images/icon_tw.svg" height="28" alt="Burn This Broadway Twitter" width="91" class="twitter-img"></a><a href="#" class="social-link w-inline-block"><img src="<?= get_template_directory_uri(); ?>/_images/icon_ig.svg" height="35" alt="Burn This Broadway Instagram" width="75" class="insta-img"></a></div>
+      <div class="social-wrapper"><a href="<?php the_field('facebook_link', 'option') ?>" class="social-link w-inline-block"><img src="<?= get_template_directory_uri(); ?>/_images/icon_fb.svg" height="35" alt="Burn This Broadway Facebook" width="34.5" class="fb-img"></a>
+
+      <a href="<?php the_field('twitter_link', 'option') ?>" class="social-link w-inline-block"><img src="<?= get_template_directory_uri(); ?>/_images/icon_tw.svg" height="28" alt="Burn This Broadway Twitter" width="91" class="twitter-img"></a>
+
+
+      <a href="<?php the_field('instagram_link', 'option') ?>" class="social-link w-inline-block"><img src="<?= get_template_directory_uri(); ?>/_images/icon_ig.svg" height="35" alt="Burn This Broadway Instagram" width="75" class="insta-img"></a>
+
+
+</div>
       <div class="amex-wrapper"><img src="<?= get_template_directory_uri(); ?>/_images/amex_lockup.png" width="175" alt="American Express Proud Partner of Burn This" class="amex-img"></div>
     </div>
   </div>
