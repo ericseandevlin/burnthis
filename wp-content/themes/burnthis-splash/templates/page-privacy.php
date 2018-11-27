7148bf042192176975aed2573b479c9edc843763
rachelle <leahrachelle.w@gmail.com>
2018-11-27 10:45:05 -0500

    Privacy Policy

--- wp-content/themes/burnthis-splash/templates/page-privacy.php
+++ wp-content/themes/burnthis-splash/templates/page-privacy.php
@@ -8,16 +8,16 @@
 <?php get_header(); ?>
 
 <div class="main-content">
-  <div class="container w-container">
 
     <?php if (have_posts()) :  while (have_posts()) : the_post(); ?>
 
       <h1 class="privacy-title"><?php the_title(); ?></h1>
+<div class="privacy-wrapper">
       <?php the_content(); ?>
+</div>
 
     <?php endwhile; endif; ?>
 
-  </div>
 
 </div>
 
