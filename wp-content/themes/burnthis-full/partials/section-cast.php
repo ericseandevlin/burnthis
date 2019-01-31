68c46f0c44b52037aea14ef86122aacb7e5c7bb0
Rachelle White <rachellew@hellorpm.com>
2019-01-31 15:53:57 -0500

    Add cast loop styles

--- wp-content/themes/burnthis-full/partials/section-cast.php
+++ wp-content/themes/burnthis-full/partials/section-cast.php
@@ -7,6 +7,8 @@
  ?>
 
 <div id="cast" class="cast-section">
+  <div class="cast-content-holder">
+
       <?php // CAST LOOP ?>
       <?php if( $cast_members ): ?>
 
@@ -25,27 +27,21 @@
 
             <?php
               $cast_row_counter++;
-              if( $cast_row_counter == 1 || $cast_row_counter % 6 == 1 ) {
-                echo '<div class="cast-row w-row">';
+              if( $cast_row_counter == 1 || $cast_row_counter % 4 == 1 ) {
+                echo '<div class="cast-block-holder">';
               }
             ?>
 
-            <div class="column w-col w-col-2 w-col-tiny-4 w-col-small-4">
-              <div class="cast-column">
+                <div class="cast-block">
                 <div class="cast-image-holder">
-                  <div class="cast-image-circle"></div>
-                  <div class="cast-hover">
-                    <p class="cast-hover-text">VIEW<br>BIO</p>
-                  </div>
 
                     <?php the_post_thumbnail( 'thumbnail', array( 'class' => 'cast-image  cast-image-hover', 'alt' => 'Image of cast member ' . get_the_title()) ); ?>
 
                 </div>
 
-                <p class="cast-name"><?php the_title(); ?></p>
-
+                <h3 class="cast-name"><?php the_title(); ?></h3>
+                <h4 class="cast-role"><?php the_field('role'); ?></h4>
               </div>
-            </div>
             <?php if( $cast_row_counter % 6 == 0 || $counter == ($length - 1) ){  echo '</div>';
             }
               $counter++;
@@ -55,6 +51,7 @@
             <?php endforeach; ?>
           <?php wp_reset_postdata(); ?>
         <?php endif; ?>
+      </div>
 
     </div>
     <div class="container w-container creative-part">
@@ -93,69 +90,3 @@
 
   </div>
 </div>
-
-<!--
-<div id="cast" class="cast-section">
-  <div class="cast-content-holder">
-    <h1 class="section-title">cast<br>   <span class="gold">/</span> creative</h1>
-    <div class="cast-block-holder">
-      <div class="cast-block"><a href="#" class="cast-photo w-inline-block"><img src="images/287.jpeg" width="300" alt="" class="image"></a>
-        <div class="cast-bio-holder">
-          <h3 class="cast-name">Adam Driver</h3>
-          <h4 class="cast-role">pale</h4>
-        </div>
-      </div>
-      <div class="cast-block"><a href="#" class="cast-photo w-inline-block"><img src="images/287.jpeg" width="300" alt="" class="image"></a>
-        <div class="cast-bio-holder">
-          <h3 class="cast-name">keri russell</h3>
-          <h4 class="cast-role">anna mann</h4>
-        </div>
-      </div>
-      <div class="cast-block"><a href="#" class="cast-photo w-inline-block"><img src="images/287.jpeg" width="300" alt="" class="image"></a>
-        <div class="cast-bio-holder">
-          <h3 class="cast-name">david furr</h3>
-          <h4 class="cast-role">burton</h4>
-        </div>
-      </div>
-      <div class="cast-block"><a href="#" class="cast-photo w-inline-block"><img src="images/287.jpeg" width="300" alt="" class="image"></a>
-        <div class="cast-bio-holder">
-          <h3 class="cast-name">brandon<br>uranowitz</h3>
-          <h4 class="cast-role">larry</h4>
-        </div>
-      </div>
-    </div>
-    <div class="creative-block-holder">
-      <div class="creative-block"><a href="#" class="cast-photo w-inline-block"></a>
-        <h3 class="creative-name">people person</h3>
-        <h4 class="creative-role">role</h4>
-      </div>
-      <div class="creative-block"><a href="#" class="cast-photo w-inline-block"></a>
-        <h3 class="creative-name">people person</h3>
-        <h4 class="creative-role">role</h4>
-      </div>
-      <div class="creative-block"><a href="#" class="cast-photo w-inline-block"></a>
-        <h3 class="creative-name">people person</h3>
-        <h4 class="creative-role">role</h4>
-      </div>
-      <div class="creative-block"><a href="#" class="cast-photo w-inline-block"></a>
-        <h3 class="creative-name">people person</h3>
-        <h4 class="creative-role">role</h4>
-      </div>
-      <div class="creative-block"><a href="#" class="cast-photo w-inline-block"></a>
-        <h3 class="creative-name">people person</h3>
-        <h4 class="creative-role">role</h4>
-      </div>
-      <div class="creative-block"><a href="#" class="cast-photo w-inline-block"></a>
-        <h3 class="creative-name">people person</h3>
-        <h4 class="creative-role">role</h4>
-      </div>
-      <div class="creative-block"><a href="#" class="cast-photo w-inline-block"></a>
-        <h3 class="creative-name">people person</h3>
-        <h4 class="creative-role">role</h4>
-      </div>
-      <div class="cast-block"><a href="#" class="cast-photo w-inline-block"></a></div>
-      <div class="cast-block"><a href="#" class="cast-photo w-inline-block"></a></div>
-      <div class="cast-block"><a href="#" class="cast-photo w-inline-block"></a></div>
-    </div>
-  </div>
-</div> -->
