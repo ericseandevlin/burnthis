a88325000d26098959fa780287b9aa02a21121cc
Rachelle White <rachellew@hellorpm.com>
2019-02-04 14:47:42 -0500

    email signup

--- wp-content/themes/burnthis-full/footer.php
+++ wp-content/themes/burnthis-full/footer.php
@@ -7,17 +7,12 @@
 
       <div class="email-wrapper">
         <div class="email-title">SIGN UP FOR NEWS ANd UPDATES</div>
-        <div class="form-block-2 w-form">
-          <form id="email-form" name="email-form" data-name="Email Form" class="form-2"><input type="text" class="text-field w-input" maxlength="256" name="email-2" data-name="Email 2" placeholder="yourname@email.com" id="email-2" required="">
-            <div class="submit-holder"><input type="submit" value="SUBMIT" data-wait="Please wait..." class="submit-button w-button"></div>
-          </form>
-          <div class="w-form-done">
-            <div>Thank you! Your submission has been received!</div>
-          </div>
-          <div class="w-form-fail">
-            <div>Oops! Something went wrong while submitting the form.</div>
-          </div>
-        </div><a href="#" class="w-inline-block"><img src="<?= get_template_directory_uri(); ?>/_images/Hudson-Theatre-Logo-Horizontal-Bodoni-MT_GRAY_White.svg" width="300" alt=""></a>
+
+        <?php include(locate_template('partials/signup.php')); ?>
+
+
+
+      <a href="#" class="w-inline-block"><img src="<?= get_template_directory_uri(); ?>/_images/Hudson-Theatre-Logo-Horizontal-Bodoni-MT_GRAY_White.svg" width="300" alt=""></a>
         <div class="address-holder">141 w 44th st, new york, ny 10036</div>
         <div class="colophon-holder">2019 burn this all rights reserved | <a href="#" class="link-4">privacy policy</a><br>‍</div>
       </div>
