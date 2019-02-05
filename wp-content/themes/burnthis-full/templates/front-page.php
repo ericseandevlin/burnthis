2cb1f62838269f065b599bbd8dddd5d3ac179b0b
Rachelle White <rachellew@hellorpm.com>
2019-02-05 14:56:40 -0500

    Naming sections

--- wp-content/themes/burnthis-full/templates/front-page.php
+++ wp-content/themes/burnthis-full/templates/front-page.php
@@ -18,71 +18,71 @@
 
     <?php include(locate_template('partials/section-about.php')); ?>
 
-    <div data-w-id="6e1af805-4ca2-9a04-367f-125ae80312ed" class="home-section section-2">
+      <div class="image-section-1">
       <div class="html-embed w-embed"><svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 100 100" preserveaspectratio="none" style=" position: absolute;
       top: 0;
       width: 100%;
       height: 10vw;">
-    <polygon fill="#000000" points="0 0, 0 0, 100 0 ,100 100">
-    </polygon></svg></div>
+      <polygon fill="#000000" points="0 0, 0 0, 100 0 ,100 100">
+      </polygon></svg></div>
       <div class="w-embed"><svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 100 100" preserveaspectratio="none" style=" position: absolute;
       bottom: 0;
       width: 100%;
       height: 10vw;">
-    <polygon fill="#000000" points="0 100,0 0,100 100,0 100">
-    </polygon></svg></div>
-    </div>
+      <polygon fill="#000000" points="0 100,0 0,100 100,0 100">
+      </polygon></svg></div>
+      </div>
 
     <?php include(locate_template('partials/section-tickets.php')); ?>
 
-    <div class="home-section section-4">
+      <div class="image-section-2">
       <div class="html-embed w-embed"><svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 100 100" preserveaspectratio="none" style=" position: absolute;
       top: 0;
       width: 100%;
       height: 10vw;">
-    <polygon fill="#000000" points="0 0, 0 0, 100 0 ,100 100">
-    </polygon></svg></div>
+      <polygon fill="#000000" points="0 0, 0 0, 100 0 ,100 100">
+      </polygon></svg></div>
       <div class="w-embed"><svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 100 100" preserveaspectratio="none" style=" position: absolute;
       bottom: 0;
       width: 100%;
       height: 10vw;">
-    <polygon fill="#000000" points="0 100,0 0,100 100,0 100">
-    </polygon></svg></div>
-    </div>
+      <polygon fill="#000000" points="0 100,0 0,100 100,0 100">
+      </polygon></svg></div>
+      </div>
 
     <?php include(locate_template('partials/section-cast.php')); ?>
 
-    <div class="home-section section-6">
+      <div class="image-section-3">
       <div class="html-embed w-embed"><svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 100 100" preserveaspectratio="none" style=" position: absolute;
       top: 0;
       width: 100%;
       height: 10vw;">
-    <polygon fill="#000000" points="0 0, 0 0, 100 0 ,100 100">
-    </polygon></svg></div>
+      <polygon fill="#000000" points="0 0, 0 0, 100 0 ,100 100">
+      </polygon></svg></div>
       <div class="w-embed"><svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 100 100" preserveaspectratio="none" style=" position: absolute;
       bottom: 0;
       width: 100%;
       height: 10vw;">
-    <polygon fill="#dca826" points="0 100,0 0,100 100,0 100">
-    </polygon></svg></div>
-    </div>
+      <polygon fill="#dca826" points="0 100,0 0,100 100,0 100">
+      </polygon></svg></div>
+      </div>
 
     <?php include(locate_template('partials/section-news.php')); ?>
 
-    <div class="home-section section-8">
+      <div class="blank-section section-8">
       <div class="html-embed w-embed"><svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 100 100" preserveaspectratio="none" style=" position: absolute;
       top: 0;
       width: 100%;
       height: 10vw;">
-    <polygon fill="#dca826" points="0 0, 0 0, 100 0 ,100 100">
-    </polygon></svg></div>
+      <polygon fill="#dca826" points="0 0, 0 0, 100 0 ,100 100">
+      </polygon></svg></div>
       <div class="w-embed"><svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 100 100" preserveaspectratio="none" style=" position: absolute;
       bottom: 0;
       width: 100%;
       height: 10vw;">
-    <polygon fill="#000000" points="0 100,0 0,100 100,0 100">
-    </polygon></svg></div>
-    </div>
+      <polygon fill="#000000" points="0 100,0 0,100 100,0 100">
+      </polygon></svg></div>
+      </div>
 
     <?php include(locate_template('partials/section-faq.php')); ?>
 
