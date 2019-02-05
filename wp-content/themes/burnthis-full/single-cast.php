b42f6d3c591ab02feebb916065089104cb130f64
Rachelle White <rachellew@hellorpm.com>
2019-02-05 13:05:51 -0500

    Cast social

--- wp-content/themes/burnthis-full/single-cast.php
+++ wp-content/themes/burnthis-full/single-cast.php
@@ -16,30 +16,36 @@
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
         <div class="bio-text"><?php the_content(); ?>
-            <?php if( $social['facebook'] ) : ?>
-              <a href="<?= $social['facebook'] ?>" class="cast-modal-social-icon w-inline-block" target="_blank" rel="noopener">
-                <img class="cast-modal-social-icon-img" src="<?= get_template_directory_uri() ?>/_images/icon_fb.svg">
-              </a>
-            <?php endif ?>
-            <?php if( $social['twitter'] ) : ?>
-              <a href="<?= $social['twitter'] ?>" class="cast-modal-social-icon w-inline-block" target="_blank" rel="noopener">
-                <img class="cast-modal-social-icon-img" src="<?= get_template_directory_uri() ?>/_images/icon_tw.svg">
-              </a>
-            <?php endif ?>
-            <?php if( $social['instagram'] ) : ?>
-              <a href="<?= $social['instagram'] ?>" class="cast-modal-social-icon w-inline-block" target="_blank" rel="noopener">
-                <img class="cast-modal-social-icon-img" src="<?= get_template_directory_uri() ?>/_images/icon_ig.svg">
-              </a>
-            <?php endif ?>
-            <?php if( $social['website'] ) : ?>
-              <a href="<?= $social['website'] ?>" class="cast-modal-social-icon w-inline-block" target="_blank" rel="noopener">
-                <img class="cast-modal-social-icon-img" src="<?= get_template_directory_uri() ?>/img/icon_website.svg">
-              </a>
-            <?php endif; ?>
+
         </div>
       <?php endwhile; else : ?>
           <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
           <?php endif; ?>
+          <?php
+          $social = get_field('social');
+           ?>
+        <div class="cast-social-wrapper">
+          <?php if( $social['facebook'] ) : ?>
+            <a href="<?= $social['facebook'] ?>" class="social-link w-inline-block" target="_blank" rel="noopener">
+              <img src="<?= get_template_directory_uri() ?>/_images/icon_fb.png" class="fb-img">
+            </a>
+          <?php endif ?>
+          <?php if( $social['twitter'] ) : ?>
+            <a href="<?= $social['twitter'] ?>" class="social-link w-inline-block" target="_blank" rel="noopener">
+              <img class="twitter-img" src="<?= get_template_directory_uri() ?>/_images/icon_tw.png" class="twitter-img">
+            </a>
+          <?php endif ?>
+          <?php if( $social['instagram'] ) : ?>
+            <a href="<?= $social['instagram'] ?>" class="social-link w-inline-block" target="_blank" rel="noopener">
+              <img class="insta-img" src="<?= get_template_directory_uri() ?>/_images/icon_ig.png" class="insta-img">
+            </a>
+          <?php endif ?>
+          <?php if( $social['website'] ) : ?>
+            <a href="<?= $social['website'] ?>" class="social-link w-inline-block" target="_blank" rel="noopener">
+              <img src="<?= get_template_directory_uri() ?>/_images/icon_website.png" class="website-img">
+            </a>
+          <?php endif; ?>
+        </div>
         <a href="<?= home_url(); ?>/#cast" class="link-block w-inline-block">
           <div>&lt; Back</div>
         </a>
